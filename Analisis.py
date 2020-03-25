33745c9ed307096944239aa7ab3276e2e4274423
faturita <rramele@gmail.com>
2020-03-25 17:09:14 -0300

    Upload the yml with the environment

---
+++ Analisis.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np 
+import pandas as pd 
+import glob
+import json
+
+
+# In[2]:
+
+
+#Indico la ruta donde se encuentra el set de datos descargados
+root_path = './data'
+
+#archivo con metadata
+metadata_path = f'{root_path}/metadata.csv'
+
+#leo el archivo con metadata
+meta_df = pd.read_csv(metadata_path, dtype={
+    'pubmed_id': str,
+    'Microsoft Academic Paper ID': str, 
+    'doi': str
+})
+meta_df.head()
+
+
+# In[3]:
+
+
+#armo un listado con los archivos json que tienen las noticias
+all_json = glob.glob(f'{root_path}/**/*.json', recursive=True)
+len(all_json)
+
+
+# In[4]:
+
+
+#creo un dataframe donde voy a guardar las columnas de interes:
+#sha: id de la noticia
+#title: titulo de la noticia
+#body_text: cuerpo de la noticia
+df = pd.DataFrame(columns = ['sha', 'title', 'abstract', 'body_text']) 
+
+
+# In[5]:
+
+
+#Leo cada json y lo agrego al dataframe
+for json_path in all_json:
+   with open(json_path) as file:
+            content = json.load(file)
+            sha = content['paper_id']
+            title = content['metadata']['title']
+            abstract = []
+            body_text = []
+            # Abstract
+            for entry in content['abstract']:
+                abstract.append(entry['text'])
+            # Body text
+            for entry in content['body_text']:
+                body_text.append(entry['text'])
+            abstract = '\n'.join(abstract)
+            body_text = '\n'.join(body_text)
+            row = [(sha, title, abstract, body_text)]
+            dfRow = pd.DataFrame(row, columns = ['sha', 'title', 'abstract', 'body_text'])
+            df = pd.concat([dfRow, df], ignore_index=True, sort=False)
+
+ 
+
+
+# In[10]:
+
+
+#muestro el contenido del dataframe
+df
+
+
+# In[19]:
+
+
+meta_df_not_null = meta_df[meta_df.sha.notnull()] 
+
+
+# In[24]:
+
+
+meta_df['sha']=="d13a685f861b0f1ba05afa6e005311ad1820fd3a"
+
+
+# In[17]:
+
+
+duplicateRowsDF = meta_df_not_null[meta_df_not_null.duplicated(['sha'])]
+
+
+# In[18]:
+
+
+duplicateRowsDF
+
+
+# In[13]:
+
+
+result = pd.merge(df,
+                 meta_df,
+                 on='sha', 
+                 how='left')
+
+
+
+
+
+# %%
--- README.md
+++ README.md
@@ -2,3 +2,45 @@
 
 This repository contains information to tackle https://www.kaggle.com/allen-institute-for-ai/CORD-19-research-challenge
 
+How to prepare my environment to start to collaborate ??
+
+* Install Git Bash
+  https://git-scm.com/downloads
+  
+* Install Anaconda
+
+  Download Anaconda 3.7 for your platform and install it.
+  https://www.anaconda.com/distribution/
+ 
+ * Install Visual Studio Code
+ 
+  Download and install it from https://code.visualstudio.com/download
+  
+ * Clone this repository
+ 
+  From the Git Bash console, run
+  
+ ```bash
+ git clone https://github.com/ezequielma20/covid19-challenge.git
+ ```
+ 
+ * Run an Anaconda Prompt
+ * Create the environment with:
+ 
+ ```bash 
+ conda env update --name nlp --file environment.yml
+ ```
+
+ (or this one if any error occurs
+ 
+ ```
+ conda env update --prefix ./env --file environment.yml 
+ ```
+
+ )
+ 
+  * Activate the newly created environment
+  ```bash
+  conda activate nlp
+  ```
+
