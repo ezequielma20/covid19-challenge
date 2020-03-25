9dcbe88cfb3e6606c53e274bdfefa0959d974649
faturita <rramele@gmail.com>
2020-03-25 18:18:21 -0300

    Upload a sample tokenizer with spacy.

---
+++ tokenizer.py
@@ -0,0 +1,8 @@
+import spacy
+
+# python -m spacy download en_core_web_sm
+
+nlp = spacy.load("en_core_web_sm")
+doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
+for token in doc:
+    print(token.text, token.pos_, token.dep_)
